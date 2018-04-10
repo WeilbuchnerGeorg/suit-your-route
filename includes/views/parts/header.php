6081c7da869f6db12f23ce26a066e1483b12c266
Dennis Pockstaller <mail@dpockstaller.at>
2018-04-10 13:21:42 +0200

    view header modified

--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -30,45 +30,31 @@
 </head>
 <body>
 <header>
-	<div class="inner">
-		<div class="logo">
-			<div class="name">Adressverwaltung</div>
-			<div class="version">1.0</div>
-		</div>
-
-		<?php if(LOGGED_IN == true): ?>
-			<nav class="navbar navbar-default">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a href="/" class="navbar-brand active">Adressverwaltung</a>
-					</div>
-
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-						<ul class="nav navbar-nav navbar-right">
-							<li><a href="logout">(Abmelden)</a></li>
-						</ul>
-
-						<p class="navbar-text navbar-right">Angemeldet als <strong class="username"><?php echo $this->username; ?></strong></p>
-
-					</div>
-				</div>
-			</nav>
-		<?php else: ?>
-			<nav class="mainnav">
-				<ul class="nav nav-pills">
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">
+          myguide
+        </a>
+      </div>
+      
+      <?php if(LOGGED_IN == true): ?>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" <?php if($this->current == "uebersicht"): ?>class="active"<?php endif; ?>>Startseite</a></li>
+        <?php if(LOGGED_IN == true): ?>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mein Profil <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="myguide_profil.html">Profil</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="logout.html">Logout</a></li>
+            </ul>
+          </li>
+        <?php else: ?>
 					<li<?php if($this->current == "login"): ?>class="active"<?php endif; ?>><a href="login">Login</a></li>
-				</ul>
-			</nav>
-		<?php endif; ?>
+        <?php endif; ?>
 
-	</div>
+      </ul> 
+    </div>
+  </nav>
 </header>
