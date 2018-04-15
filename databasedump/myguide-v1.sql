46fbfec1b6749fb72b7bfaddc849d725436b638c
Weilbuchner Georg <georg.weilb@gmeil.com>
2018-04-15 21:59:43 +0200

    Tabellen mit Daten befüllen

--- databasedump/myguide-v1.sql
+++ databasedump/myguide-v1.sql
@@ -62,3 +62,64 @@ CREATE TABLE IF NOT EXISTS `tour` (
   FOREIGN KEY(ratingid) REFERENCES rating(id),
   FOREIGN KEY(activityid) REFERENCES activity(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+TRUNCATE TABLE `tour`;
+INSERT INTO `tour` (`id`, `name`, `description`, `date`, `duration`, `startlocation`, `image`, `userid`, `regionid`, `difficultyid`, `ratingid`, `activityid`) VALUES
+(1, '', '', , , '', '', , , , ,);
+
+TRUNCATE TABLE `user`;
+INSERT INTO `user` (`id`, `name`, `password`, `firstname`, `lastname`, `image`) VALUES
+(1,'Benutzername','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq', 'Josef','Brombeer', '')
+  ,(2,'Georg','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Georg','Weilbuchner', '')
+	,(3,'Lisa','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Lisa', 'Pedevilla', '')
+	,(4,'Manuel','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Manuel','Danninger', '')
+	,(5,'Julia','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Julia','Lasser', '')
+	,(6,'Fritz','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Fritz','Gollner', '')
+	,(7,'Tobias','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Tobias','Aberer', '')
+	,(8,'Gerald','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Gerald','Webersberger', '')
+	,(9,'Christina','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Christina','Eder', '')
+	,(10,'Gerlinde','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Gerlinde','Webersberger', '')
+	,(11,'Albert','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Albert','Webersberger', '')
+	,(12,'Andrea','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Andrea','Stich', '')
+	,(13,'Marco','$2y$10$7Z3Iq0Zl.WIiNH7t8bk39OYVp3T6ICb2dF7yYAhhEaUMiFKbh3apq','Marco','Wilhelm', '');
+
+TRUNCATE TABLE `region`;
+INSERT INTO `region` (`id`, `name`) VALUES
+	 (1, 'Wilder Kaiser')
+	,(2, 'Kitzbuehler Alpen')
+	,(3, 'Berchtesgadener Alpen')
+	,(4, 'Salzkammergut');
+
+TRUNCATE TABLE `difficulty`;
+INSERT INTO `difficulty` (`id`, `name`) VALUES
+   (1, 'sehr einfach')
+	,(2, 'einfach')
+	,(3, 'mittel')
+	,(4, 'schwer')
+	,(5, 'sehr schwer');
+
+TRUNCATE TABLE `rating`;
+INSERT INTO `rating` (`id`, `value`) VALUES
+   (1, 1)
+	,(2, 2)
+	,(3, 3)
+	,(4, 4)
+	,(5, 5);
+
+TRUNCATE TABLE `activity`;
+INSERT INTO `activity` (`id`, `name`) VALUES
+	 (1,'Wandern')
+	,(2,'Klettern')
+	,(3,'Mountainbiken')
+	,(4,'Spaziergang')
+	,(5,'Bike and Hike')
+	,(6,'Bergsteigen')
+	,(7,'Rennradfahren')
+	,(8,'Eisklettern')
+	,(9,'Rodeln')
+	,(10,'Skitouren')
+	,(11,'Schneeschuhwandern')
+	,(12,'Skifahren')
+	,(13,'Langlaufen')
+	,(14,'Laufen')
+	,(15,'Walken');
