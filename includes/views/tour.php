0ec72ee4d7d92b24ea719a13e1d81b7670f1a223
Weilbuchner Georg <georg.weilb@gmeil.com>
2018-04-24 11:07:26 +0200

    Tour Formular bearbeiten

--- includes/views/tour.php
+++ includes/views/tour.php
@@ -1,25 +1,57 @@
 <div class="row">
-    <form method="<?php if($this->id): ?>put<?php else: ?>post<?php endif; ?>" action="api/tours/" class="col-xs-12">
+    <form method="<?php if($this->id): ?>put<?php else: ?>post<?php endif; ?>" action="api/tour/" class="col-xs-12">
 
         <div class="form-group">
             <label for="name">Name:</label>
             <input type="text" name="name" class="form-control" id="name" value="<?php echo $this->name; ?>">
         </div>
         <div class="form-group">
-            <label for="description">Beschreibung:</label>
-            <input type="text" name="description" class="form-control" id="description" value="<?php echo $this->lastname; ?>">
-        </div>
-        <div class="form-group">
             <label for="date">Datum:</label>
             <input type="date" class="form-control" name="date" id="date" value="<?php echo $this->date; ?>">
+        </div>
+        <div class="form-group">
+            <label for="duration">Bewertung:</label>
+            <select class="form-control center-block">
+                <?php foreach($this->rating as $rating): ?>
+                    <option value="<?php echo $rating->id; ?>"><?php echo $region->value; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="duration">Region:</label>
+            <select class="form-control center-block">
+                <?php foreach($this->regions as $region): ?>
+                    <option value="<?php echo $region->id; ?>"><?php echo $region->name; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="startlocation">Startort:</label>
+            <input type="text" name="startlocation" class="form-control" id="startlocation" value="<?php echo $this->startlocation; ?>">
+        </div>
+        <div class="form-group">
+            <label for="duration">Aktivität:</label>
+            <select class="form-control center-block">
+                <?php foreach($this->activities as $activity): ?>
+                    <option value="<?php echo $activity->id; ?>"><?php echo $activity->name; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="duration">Schwierigkeit:</label>
+            <select class="form-control center-block">
+                <?php foreach($this->difficulties as $difficulty): ?>
+                    <option value="<?php echo $difficulty->id; ?>"><?php echo $difficulty->name; ?></option>
+                <?php endforeach; ?>
+            </select>
         </div>
         <div class="form-group">
             <label for="duration">Dauer:</label>
             <input type="time" name="duration" class="form-control" id="duration" value="<?php echo $this->duration; ?>">
         </div>
         <div class="form-group">
-            <label for="startlocation">Startort:</label>
-            <input type="text" name="startlocation" class="form-control" id="startlocation" value="<?php echo $this->startlocation; ?>">
+            <label for="description">Beschreibung:</label>
+            <input type="text" name="description" class="form-control" id="description" value="<?php echo $this->lastname; ?>">
         </div>
         <?php if($this->id): ?>
             <input type="hidden" name="id" value="<?php echo $this->id; ?>">
